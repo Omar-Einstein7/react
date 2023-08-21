622f6f3bde145caff63687c9cd6a9cc86307e058
Omar-Einstein7 <omarhan113355@gmail.com>
2023-08-22 00:46:11 +0300

    react

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View ,TextInput,Button , Image ,SafeAreaView} from 'react-native';
 
 export default function App() {
+  const [inputText, setInputText] = useState('');
+  const [displayText, setDisplayText] = useState('');
+  const img = "https://www.pngplay.com/wp-content/uploads/13/Shawn-Mendes-Transparent-File.png"
+  const handleInputChange = (text) => {
+    setInputText(text);
+  };
+
+
+  const handleDisplayButtonPress = () => {
+    setDisplayText("good "+inputText+" is the Canidian Singer");
+  };
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{flex :20}}> 
+    <View style={ styles.container }>
+
+    <Text style={{ margin: 30 ,fontSize: 20}} >WHO IS THAT Singer ?</Text>
+    <TextInput
+      style={{ height: 40, borderColor: 'black', borderWidth: 2, marginBottom: 20 , marginHorizontal: 5 ,
+      borderBottomRightRadius: 10,
+       borderBottomLeftRadius:10,
+      borderTopRightRadius: 10,
+      borderTopLeftRadius: 10
+    }}
+      onChangeText={handleInputChange}
+      value={inputText}
+    />
+    <Button
+      title=  " Display "
+      onPress={handleDisplayButtonPress}
+    />
+    <Text>{displayText}</Text>
+    <View style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center' }}>
+          <Image
+            style={{ width: 300, height: 300 }}
+            source={{ uri: img }}
+          />
+        </View>
+  </View>
+</SafeAreaView>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flex: 20,
+    padding: 16,
+    backgroundColor: 'lightblue',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
